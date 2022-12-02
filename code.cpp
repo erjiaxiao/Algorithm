614c40be246806994b51d6eb3564fce8bf9991c5
erjiaxiao <erjiaxiao@gmail.com>
2022-12-02 10:07:38 +0800

    heap application

--- code.cpp
+++ code.cpp
@@ -4,13 +4,28 @@ using namespace std;
 
 #define LOCAL
 
+class Pair
+{
+
+public:
+    int value, index;
+
+    Pair() {}
+
+    Pair(int index, int value)
+    {
+        this->index = index;
+        this->value = value;
+    }
+};
+
 class Heap
 {
 
 public:
-    vector<int> vec;
+    vector<Pair> vec;
 
-    void add(int x)
+    void add(Pair x)
     {
         vec.push_back(x);
 
@@ -18,11 +33,11 @@ public:
         while (curr != 0)
         {
             par = (curr - 1) / 2;
-            if (vec[par] <= vec[curr])
+            if (vec[par].value <= vec[curr].value)
                 return;
-            if (vec[par] > vec[curr])
+            if (vec[par].value > vec[curr].value)
             {
-                int tmp = vec[curr];
+                Pair tmp = vec[curr];
                 vec[curr] = vec[par];
                 vec[par] = tmp;
             }
@@ -30,8 +45,9 @@ public:
         }
     }
 
-    void pop()
+    Pair pop()
     {
+        Pair popping = vec[0];
         vec[0] = vec[vec.size() - 1];
         vec.pop_back();
 
@@ -45,7 +61,7 @@ public:
                 break;
             else if (child1 < vec.size() && child2 < vec.size())
             {
-                if (vec[child1] < vec[child2])
+                if (vec[child1].value < vec[child2].value)
                     next = child1;
                 else
                     next = child2;
@@ -55,16 +71,18 @@ public:
             else
                 next = child2;
 
-            if (vec[curr] <= vec[next])
+            if (vec[curr].value <= vec[next].value)
                 break;
             else
             {
-                int tmp = vec[next];
+                Pair tmp = vec[next];
                 vec[next] = vec[curr];
                 vec[curr] = tmp;
             }
             curr = next;
         }
+
+        return popping;
     }
 };
 
@@ -75,23 +93,43 @@ int main()
     freopen("data.in", "r", stdin);
 #endif
 
-    Heap heap;
-    int n, op, x;
+    int n, m;
+    cin >> n >> m;
 
-    cin >> n;
-
+    vector<vector<int>> equations;
     while (n--)
     {
-        cin >> op;
+        int a, b, c;
+        cin >> a >> b >> c;
+        vector<int> curr{a, b, c, 1};
+        equations.push_back(curr);
+    }
 
-        if (op == 1)
-        {
-            cin >> x;
-            heap.add(x);
-        }
-        else if (op == 2)
-            cout << heap.vec[0] << endl;
+    Heap heap;
+    for (int i = 0; i < equations.size(); i++)
+    {
+
+        int value = equations[i][0] * equations[i][3] * equations[i][3] +
+                    equations[i][1] * equations[i][3] +
+                    equations[i][2];
+        Pair pair(i, value);
+        heap.add(pair);
+    }
+
+    while (m--)
+    {
+
+        Pair curr = heap.pop();
+
+        if (m == 0)
+            cout << curr.value << endl;
         else
-            heap.pop();
+            cout << curr.value << " ";
+
+        equations[curr.index][3] += 1;
+        curr.value = equations[curr.index][0] * equations[curr.index][3] * equations[curr.index][3] +
+                     equations[curr.index][1] * equations[curr.index][3] +
+                     equations[curr.index][2];
+        heap.add(curr);
     }
 }
